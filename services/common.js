88de5e15f2e130286b6a8b80fb16f9ae2f7b8271
SYDALA ANJANEYA PRATAP <anjaanjaneyapratapsydala@gmail.com>
2024-01-05 17:16:51 +0530

    Done Cookies

---
+++ services/common.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+
+exports.isAuth = (req, res, done) => {
+  return passport.authenticate('jwt');
+};
+
+exports.sanitizeUser = (user) => {
+  return { id: user.id, role: user.role };
+};
+
+exports.cookieExtractor = function (req) {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies['jwt'];
+  }
+  //TODO : this is temporary token for testing without cookie
+  token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1OGJlN2QxYTc4Yjg2ZDM1MmVjY2I2NCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzA0NDEzMzMwfQ.zZiD1BQEmx8Ym7_sERqn-W7mZ35QOvVe-RGylYHGNEM"
+  return token;
+};
